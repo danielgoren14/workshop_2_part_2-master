23b5eeb98ccd7b3c73aaeaf307f80aa3681198f8
daniel <danielgoren14@gmail.com>
2023-09-14 22:57:02 +0300

    /

--- src/main/java/ImageProcess.java
+++ src/main/java/ImageProcess.java
@@ -3,8 +3,8 @@ import java.io.File;
 public class ImageProcess {
     public static String API_ENDPOINT = "https://app.seker.live/fm1/upload-file";
     public static String STUDENT_ID = "0"; //Set your ID here!
-    public static String INPUT_FILE_PATH = ""; //Set the path to the input file
-    public static String OUTPUT_FILE_PATH = ""; //Set the path to the output file
+    public static String INPUT_FILE_PATH = "https://app.seker.live/fm1/get-image?id=" + STUDENT_ID; //Set the path to the input file
+    public static String OUTPUT_FILE_PATH = "src/main/java/NewImage"; //Set the path to the output file
 
     public static void main(String[] args) {
         cleanTheNoisyImage(INPUT_FILE_PATH);
